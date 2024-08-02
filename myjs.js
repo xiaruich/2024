d89cec9b1561b1fc88c07993927394612ad60021
xiaruich <168495529+xiaruich@users.noreply.github.com>
2024-08-02 11:40:29 +0900

    Create myjs.js

---
+++ myjs.js
@@ -0,0 +1,11 @@
+alert('hello,xiarui');
+alert(1+1);
+alert(Math.sqrt(1+1));
+alert(sqrt2+sqrt3);
+alert(1/0);
+alert(0/0);
+alert(2+3);
+alert(Math.sqrt('2'+'3'));
+alert(Math.sqrt('2')+Number('3'))
+alert('2'+3);
+alert(number('2')+3)
